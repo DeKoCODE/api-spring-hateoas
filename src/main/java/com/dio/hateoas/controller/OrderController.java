123bf9ae71e3c1461ecd4c33915d98af875f2445
Denison Kolling <denisonkolling@gmail.com>
2023-03-27 15:51:46 -0300

    second commit - included cancel order and complete order

--- src/main/java/com/dio/hateoas/controller/OrderController.java
+++ src/main/java/com/dio/hateoas/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.dio.hateoas.controller;
 
 import com.dio.hateoas.entity.Order;
+import com.dio.hateoas.entity.Status;
+import com.dio.hateoas.exception.OrderNotFoundException;
 import com.dio.hateoas.repository.OrderRepository;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -70,6 +75,38 @@ public class OrderController {
     @DeleteMapping("/orders/{id}")
             void deleteOrder(@PathVariable long id){
                     repositoryOrder.deleteById(id);
+    }
+
+    //Implantação PUT para cancelar uma Ordem com Status em progresso:
+    @PutMapping("/orders/{id}/cancel")
+    public ResponseEntity<?>  cancelOrderById(@PathVariable long id){
+        Order cancelledOrder = repositoryOrder.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+        //Verifica se o status atual está em progresso:
+        if (cancelledOrder.getStatus() == Status.IN_PROGRESS){
+            cancelledOrder.setStatus(Status.CANCELLED);
+            cancelledOrder.add(linkTo(methodOn(OrderController.class).consultOneOrder(id)).withSelfRel());
+            repositoryOrder.save(cancelledOrder);
+            return new ResponseEntity<>(cancelledOrder,HttpStatus.OK);
+        }
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                    .body("You can't complete the task, the order ha a " + cancelledOrder.getStatus() + "status");
+    }
+
+
+    @PutMapping("/orders/{id}/complete")
+    public ResponseEntity<?>  completedOrderById(@PathVariable long id){
+        Order completedOrder = repositoryOrder.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+        //Verifica se o status atual está em progresso:
+        if (completedOrder.getStatus() == Status.IN_PROGRESS){
+            completedOrder.setStatus(Status.COMPLETED);
+            completedOrder.add(linkTo(methodOn(OrderController.class).consultOneOrder(id)).withSelfRel());
+            repositoryOrder.save(completedOrder);
+            return new ResponseEntity<>(completedOrder,HttpStatus.OK);
+        }
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body("You can't complete the task, the order has a " + completedOrder.getStatus() + " status");
     }
 }
 
